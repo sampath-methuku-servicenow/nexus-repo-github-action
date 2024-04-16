6b0766a235457fac8bc8069a4bd28a53dacd1290
sampath-methuku-servicenow <83061325+sampath-methuku-servicenow@users.noreply.github.com>
2024-04-16 17:18:29 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,9 +18,9 @@ export JAVA_HOME=/usr/lib/jvm/java-1.8-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 
 
+let tagName="${9}_${RANDOM}_${RANDOM}";
+
+curl -v -u "$2:$3" -X POST --header 'Content-Type: application/json' "${1}service/rest/v1/tags" -d "{ \"name\": \"$tagName\", \"attributes\": { \"repo_name\": \"$5\" }}"
 
 
-curl -v -u "$2:$3" -X POST --header 'Content-Type: application/json' "${1}service/rest/v1/tags" -d "{ \"name\": \"$9\", \"attributes\": { \"repo_name\": \"$5\" }}"
-
-
-groovy /opt/sonatype/bin/NexusPublisher.groovy --serverurl $1 --username $2 --password $3 --format $4 --repository $5 --tagname $9 --filename $GITHUB_WORKSPACE/$8 $(echo -C$6 | sed 's/ / -C/g') $(echo -A$7 | sed 's/ / -A/g') 
+groovy /opt/sonatype/bin/NexusPublisher.groovy --serverurl $1 --username $2 --password $3 --format $4 --repository $5 --tagname $tagName --filename $GITHUB_WORKSPACE/$8 $(echo -C$6 | sed 's/ / -C/g') $(echo -A$7 | sed 's/ / -A/g') 
