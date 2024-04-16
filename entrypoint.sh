3aa7f5030f16587cf49d005e2d4d2dd1b6c6e5ae
sampath-methuku-servicenow <83061325+sampath-methuku-servicenow@users.noreply.github.com>
2024-04-16 17:07:04 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,4 +23,4 @@ export PATH=$JAVA_HOME/bin:$PATH
 curl -v -u "$2:$3" -X POST --header 'Content-Type: application/json' "${1}service/rest/v1/tags" -d "{ \"name\": \"$9\", \"attributes\": { \"repo_name\": \"$5\" }}"
 
 
-groovy /opt/sonatype/bin/NexusPublisher.groovy --serverurl $1 --username $2 --password $3 --format $4 --repository $5 --filename $GITHUB_WORKSPACE/$8 $(echo -C$6 | sed 's/ / -C/g') $(echo -A$7 | sed 's/ / -A/g') --tagname $9
+groovy /opt/sonatype/bin/NexusPublisher.groovy --serverurl $1 --username $2 --password $3 --format $4 --repository $5 --tagname $9 --filename $GITHUB_WORKSPACE/$8 $(echo -C$6 | sed 's/ / -C/g') $(echo -A$7 | sed 's/ / -A/g') 
