883f8aef15a5da719abf1e79377d027a412b6ba2
sampath-methuku-servicenow <83061325+sampath-methuku-servicenow@users.noreply.github.com>
2024-04-16 16:44:46 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,17 +18,10 @@ export JAVA_HOME=/usr/lib/jvm/java-1.8-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 
 
-curl -v -u admin:admin -X POST --header 'Content-Type: application/json' http://13.54.106.6:8081/service/rest/v1/tags \
-  -d '{
-    "name": "testing123",
-    "attributes": {
-        "repo_name":"maven_repo"
-    }
-}'
 
 echo """ curl -v -u "$2:$3" -X POST --header 'Content-Type: application/json' $1/service/rest/v1/tags  -d "{ 'name': $9,'attributes': {'repo_name':$5}}"  """
 
-let payload=  "{ 'name':"+ $9+",'attributes': {'repo_name':"+$5+" }}"
+let payload="{ 'name':"+ $9+",'attributes': {'repo_name':"+$5+" }}";
 echo "Payload==$payload";
 
 curl -v -u "$2:$3" -X POST --header 'Content-Type: application/json' ${1}service/rest/v1/tags  -d "$payload"
